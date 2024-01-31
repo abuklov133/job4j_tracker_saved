a9901d434c68659640113c2fcb3e79bb02291695
abuklov133 <abuklov133@gmail.com>
2024-01-31 21:49:46 +0300

    7. Метод удаления заявки Tracker.delete [#211749 #469244]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -71,4 +71,13 @@ public class Tracker {
         }
         return result;
     }
+
+    public void delete(int id) {
+        int index = indexOf(id);
+        if (index != -1) {
+            System.arraycopy(items, index, items, index + 1, size - index - 1);
+            items[size - 1] = null;
+            size--;
+        }
+    }
 }
