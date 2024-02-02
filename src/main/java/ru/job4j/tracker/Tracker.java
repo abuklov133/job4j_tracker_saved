eaebf03ad16b4f8509c5904ea777aa0de5aa0185
abuklov133 <abuklov133@gmail.com>
2024-02-02 15:32:24 +0300

    5. Tracker - хранилище [#396 #469240]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -26,7 +26,7 @@ public class Tracker {
         Item[] result = new Item[size];
         int count = 0;
         for (int i = 0; i < size; i++) {
-            if (items[i] != null && items[i].getName().equals(key)) {
+            if (items[i].getName().equals(key)) {
                 result[count++] = items[i];
             }
         }
