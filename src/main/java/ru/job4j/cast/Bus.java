70034e8b776b89929617896bc2f89f6f0bc89198
abuklov133 <abuklov133@gmail.com>
2024-02-05 08:37:33 +0300

    4.2. Интерфейс Input [#181092 #470772]

--- src/main/java/ru/job4j/cast/Bus.java
+++ src/main/java/ru/job4j/cast/Bus.java
@@ -1,10 +1,29 @@
 package ru.job4j.cast;
 
-public class Bus implements Vehicle {
+import ru.job4j.polymorphism.Transport;
+import ru.job4j.tracker.Input;
+
+public class Bus implements Vehicle, Transport {
     private String nameClass = getClass().getSimpleName();
 
     @Override
     public void move() {
         System.out.println(nameClass + " Едет по дороге");
+    }
+
+    @Override
+    public void drive() {
+        System.out.println("Едет долго и некомфортно.");
+    }
+
+    @Override
+    public void traveler(int passenger) {
+        System.out.println("Перевозит " + passenger + " пассажиров.");
+    }
+
+    @Override
+    public double refuel(int fuel) {
+        double price = 100;
+        return fuel * price;
     }
 }
---
+++ src/main/java/ru/job4j/polymorphism/Transport.java
@@ -0,0 +1,10 @@
+package ru.job4j.polymorphism;
+
+public interface Transport {
+    void drive();
+
+    void traveler(int passenger);
+
+    double refuel(int fuel);
+
+}
